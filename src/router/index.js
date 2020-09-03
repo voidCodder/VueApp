e3384b113d76f064854212b9ad759a3254df9f92
voidCodder <pachademon.shevelev@gmail.com>
2020-09-03 21:23:54 +0300

    App with page 404

--- src/router/index.js
+++ src/router/index.js
@@ -1,30 +1,81 @@
-import Vue from "vue";
-import VueRouter from "vue-router";
-import Home from "../views/Home.vue";
+import Vue from 'vue'
+import VueRouter from 'vue-router'
 
-Vue.use(VueRouter);
+import store from '@/store'
+
+import Nprogress from 'nprogress'
+
+Vue.use(VueRouter)
 
 const routes = [
   {
-    path: "/",
-    name: "Home",
-    component: Home
+    path: '/',
+    name: 'event-list',
+    component: () => import('@/views/EventList.vue'),
+    props: true
   },
   {
-    path: "/about",
-    name: "About",
-    // route level code-splitting
-    // this generates a separate chunk (about.[hash].js) for this route
-    // which is lazy-loaded when the route is visited.
-    component: () =>
-      import(/* webpackChunkName: "about" */ "../views/About.vue")
+    path: '/event/:id',
+    name: 'event-show',
+    component: () => import('@/views/EventShow.vue'),
+    props: true,
+    beforeEnter(to, from, next) {
+      store
+        .dispatch('event/fetchEvent', to.params.id)
+        .then(event => {
+          to.params.event = event
+          next()
+        })
+        .catch(error => {
+          if (error.response && error.response.status == '404') {
+            next({
+              name: '404',
+              params: { resource: 'event' }
+            })
+          } else {
+            next({ name: 'network-issue' })
+          }
+        })
+    }
+  },
+  {
+    path: '/create',
+    name: 'event-create',
+    component: () => import('@/views/EventCreate.vue')
+  },
+  {
+    path: '/404',
+    name: '404',
+    component: () => import('@/views/NotFound.vue'),
+    props: true
+  },
+  {
+    path: '/network-issue',
+    name: 'network-issue',
+    component: () => import('@/views/NetworkIssue')
+  },
+  {
+    path: '*',
+    redirect: {
+      name: '404',
+      params: { resource: 'page' }
+    }
   }
-];
+]
 
 const router = new VueRouter({
-  mode: "history",
+  mode: 'history',
   base: process.env.BASE_URL,
   routes
-});
+})
 
-export default router;
+router.beforeEach((to, from, next) => {
+  Nprogress.start()
+  next()
+})
+
+router.afterEach(() => {
+  Nprogress.done()
+})
+
+export default router
---
+++ src/views/EventList.vue
@@ -0,0 +1,62 @@
+<template>
+  <div>
+    <h1>Events List for {{ user.user.name }}</h1>
+    <EventCard v-for="event in event.events" :key="event.id" :event="event" />
+    <template v-if="page != 1">
+      <router-link
+        :to="{ name: 'event-list', query: { page: page - 1 } }"
+        rel="prev"
+        >Prev Page</router-link
+      >
+    </template>
+    <router-link
+      v-if="hasNextPage"
+      :to="{ name: 'event-list', query: { page: page + 1 } }"
+      rel="next"
+      >Next Page</router-link
+    >
+  </div>
+</template>
+
+<script>
+import EventCard from '@/components/EventCard.vue'
+import { mapState } from 'vuex'
+
+import store from '@/store'
+
+function getPageEvents(to, next) {
+  const currentPage = parseInt(to.query.page) || 1
+  store
+    .dispatch('event/fetchEvents', {
+      page: currentPage
+    })
+    .then(() => {
+      to.params.page = currentPage
+      next()
+    })
+}
+
+export default {
+  props: {
+    page: {
+      type: Number,
+      required: true
+    }
+  },
+  components: {
+    EventCard
+  },
+  beforeRouteEnter(to, from, next) {
+    getPageEvents(to, next)
+  },
+  beforeRouteUpdate(to, from, next) {
+    getPageEvents(to, next)
+  },
+  computed: {
+    hasNextPage() {
+      return this.event.eventsTotal > this.page * this.event.perPage
+    },
+    ...mapState(['event', 'user'])
+  }
+}
+</script>
