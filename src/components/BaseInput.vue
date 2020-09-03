5d2a6b3b4f5f3066ea42f0ca8d2eb84b2cc4ff78
voidCodder <pachademon.shevelev@gmail.com>
2020-09-03 23:02:10 +0300

    Add Components BaseInput, BaseSelect

---
+++ src/components/BaseInput.vue
@@ -0,0 +1,24 @@
+<template>
+  <div>
+    <label v-if="label">{{ label }}</label>
+    <input :value="value" @input="updateEvent" v-bind="$attrs" />
+  </div>
+</template>
+
+<script>
+export default {
+  inheritAttrs: false,
+  props: {
+    label: {
+      type: String,
+      default: ''
+    },
+    value: [String, Number]
+  },
+  methods: {
+    updateEvent() {
+      this.$emit('input', event.target.value)
+    }
+  }
+}
+</script>
--- src/views/EventCreate.vue
+++ src/views/EventCreate.vue
@@ -1,47 +1,51 @@
 <template>
   <div>
     <form @submit.prevent="createEvent">
-      <label>Select a category</label>
-      <select v-model="event.category">
-        <option v-for="cat in categories" :key="cat">{{ cat }}</option>
-      </select>
+      <BaseSelect
+        label="Select a category"
+        :options="categories"
+        v-model="event.category"
+        class="field"
+      />
+
       <h3>Name & describe your event</h3>
-      <div class="field">
-        <label>Title</label>
-        <input
-          v-model="event.title"
-          type="text"
-          placeholder="Add an event title"
-        />
-      </div>
-      <div class="field">
-        <label>Description</label>
-        <input
-          v-model="event.description"
-          type="text"
-          placeholder="Add a description"
-        />
-      </div>
+      <BaseInput
+        label="Title"
+        v-model="event.title"
+        type="text"
+        placeholder="Title"
+        class="field"
+      />
+      <BaseInput
+        label="Description"
+        v-model="event.description"
+        type="text"
+        placeholder="Description"
+        class="field"
+      />
+
       <h3>Where is your event?</h3>
-      <div class="field">
-        <label>Location</label>
-        <input
-          v-model="event.location"
-          type="text"
-          placeholder="Add a location"
-        />
-      </div>
+      <BaseInput
+        label="Location"
+        v-model="event.location"
+        type="text"
+        placeholder="Add a Location"
+        class="field"
+      />
+
       <h3>When is your event?</h3>
       <div class="field">
         <label>Date</label>
         <datepicker v-model="event.date" placeholder="Select a date" />
       </div>
-      <div class="field">
-        <label>Select a time</label>
-        <select v-model="event.time">
-          <option v-for="time in times" :key="time">{{ time }}</option>
-        </select>
-      </div>
+
+      <BaseSelect
+        label="Select a time"
+        :options="times"
+        v-model="event.time"
+        class="field"
+      />
+
       <input type="submit" class="button -fill-gradient" value="Submit" />
     </form>
   </div>
